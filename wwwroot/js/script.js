188deb859c94abe457307815c48e3a38c9f57d29
Umair Syed <naqvi.umair@gmail.com>
2020-11-17 13:34:19 +0100

    design changes list implementation

---
+++ wwwroot/js/script.js
@@ -0,0 +1,10 @@
+﻿function VibrateOnTouch() {
+    var checkExist = setInterval(function () {
+        var counterBtn = document.getElementById("counterbtn");
+        console.log('checking Counter Btn');
+        if (!!counterBtn) {
+            $("#counterbtn").vibrate("long");
+            clearInterval(checkExist);
+        }
+    }, 100);
+}
